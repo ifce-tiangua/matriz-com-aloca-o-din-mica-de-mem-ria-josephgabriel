ed6f840b4713d86424fe46387605a8e0b0a4baa4
josephgabriel <josgabrielsoares825@gmail.com>
2024-11-17 12:31:49 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,30 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main(){
+//Bem-vindo ao Exercício "Vetor Dinâmico"
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+//Neste exercício, você vai escrever um programa que:
 
-    return 0;
+int main()
+{
+	int i;
+	int valor;
+	int t; //Leia o tamanho de um vetor
+
+	printf("Digite  tamanho do vetor: ");
+	scanf("%d", &t);
+
+	int*p = (int*)malloc(t*sizeof(int)); //Aloque dinamicamente a memória para o vetor
+
+    for(i=0; i<t; i++){
+       printf("digite os dados do vetor %d: ", i);//Leia os dados de um vetor
+       scanf("%d", &p[i]);
+	}
+
+    for(i=0; i<t; i++){
+    	printf("[%d] = %d\n",i, p[i]); //Apresente os dados do vetor
+	}
+
+      free(p); //Desaloque a memória alocada anteriormente
+
 }
