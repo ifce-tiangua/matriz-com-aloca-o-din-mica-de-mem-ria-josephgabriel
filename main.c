86ddf6e7da847fad742498c8e08cf6e250059e4c
josephgabriel <josgabrielsoares825@gmail.com>
2024-11-19 19:03:54 -0300

    Update main.c

--- main.c
+++ main.c
@@ -20,24 +20,25 @@ int main() {
             }
         }
 
+        
         for (int i = 0; i < L; i++) {
             for (int j = 0; j < C; j++) {
+                
                 if (i == 0 || i == L - 1 || j == 0 || j == C - 1) {
                     printf("%d", matriz[i * C + j]);
+
+                   
                     if (j < C - 1) {
                         printf(" ");
-                    }else{
-                        
-                    printf("\\n");
-
+                    } else if (i < L - 1 || j < C - 1) {
+                       
+                        printf("\\n");
                     }
                 }
             }
-            
         }
     }
 
     free(matriz);
-
     return 0;
 }
