1b8b21feb892d8c7ee79190222ae3042eb0ff213
josephgabriel <josgabrielsoares825@gmail.com>
2024-11-19 19:08:22 -0300

    Update main.c

--- main.c
+++ main.c
@@ -9,34 +9,37 @@ int main() {
 
     matriz = malloc(L * C * sizeof(int));
 
+
     if (L == 0 || C == 0) {
         printf("[matriz vazia]\n");
         free(matriz);
-        return 1;
+        return 0;
     } else {
+
         for (int i = 0; i < L; i++) {
             for (int j = 0; j < C; j++) {
                 scanf("%d", &matriz[i * C + j]);
             }
         }
 
-        
+
         for (int i = 0; i < L; i++) {
             for (int j = 0; j < C; j++) {
-                
+
                 if (i == 0 || i == L - 1 || j == 0 || j == C - 1) {
                     printf("%d", matriz[i * C + j]);
 
-                   
                     if (j < C - 1) {
                         printf(" ");
-                    } else if (i < L - 1 || j < C - 1) {
-                       
+                    }
+
+                    if (j == C - 1 && i < L - 1) {
                         printf("\\n");
                     }
                 }
             }
         }
+        printf("\n");
     }
 
     free(matriz);
