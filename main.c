a28956615636a68953d6fee147646f1dda1613f3
josephgabriel <josgabrielsoares825@gmail.com>
2024-11-19 20:09:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,51 +2,49 @@
 #include <stdlib.h>
 
 int main() {
-    int L, C, *matriz;
+    int L, C, i, j;
 
-
-    scanf("%d", &L);
-    scanf("%d", &C);
-
-
-    matriz = malloc(L * C * sizeof(int));
+    scanf("%d %d", &L, &C);
 
     if (L == 0 || C == 0) {
-
-        printf("[matriz vazia]");
-        free(matriz);
+        printf("[matriz vazia]\n");
         return 0;
-        } else {
+    }
 
-        for (int i = 0; i < L; i++) {
-        for (int j = 0; j < C; j++) {
-        scanf("%d", &matriz[i * C + j]);
+    int **matriz = malloc(L * sizeof(int*));
+    if (matriz == NULL) {
 
+        return 1;
+    }
+
+    for (i = 0; i < L; i++) {
+        matriz[i] = malloc(C * sizeof(int));
+        if (matriz[i] == NULL) {
+
+            return 1;
+        }
+    }
+
+    for (i = 0; i < L; i++) {
+        for (j = 0; j < C; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    for (i = 0; i < L; i++) {
+        for (j = 0; j < C; j++) {
+            printf("%d", matriz[i][j]);
+            if (j < C - 1) {
+                printf(" ");
             }
         }
+        printf("\n");
+    }
 
-
-        for (int i = 0; i < L; i++) {
-        for (int j = 0; j < C; j++) {
-        if (i == 0 || i == L - 1 || j == 0 || j == C - 1)
-
-        {
-
-        printf("%d", matriz[i * C + j]);
-
-        if (j < C - 1)
-            {printf(" ");}
-
-        }
-                                   }
-
-        if (i < L - 1)
-            {printf("\\n");}
-
-                                   }
-               }
-
-
+    for (i = 0; i < L; i++) {
+        free(matriz[i]);
+    }
     free(matriz);
+
     return 0;
 }
